4fe59ee43a1dd3e692b20b1eb21c94303b1cf7c9
NickyParsons <79032032193@ya.ru>
2021-04-20 16:33:44 +0300

    Lesson 2 commit 1

---
+++ Lesson_2_1/LinkedList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_2_1
+{
+    class LinkedList : ILinkedList
+    {
+        private Node head;
+        private Node tail;
+        public void AddNode(int value)
+        {
+            Node newNode = new Node();
+            newNode.Value = value;
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                newNode.PrevNode = this.tail;
+                this.tail.NextNode = newNode;
+            }
+            this.tail = newNode;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            Node newNode = new Node();
+            newNode.Value = value;
+            if (node.NextNode == null)
+            {
+                newNode.PrevNode = node;
+                node.NextNode = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                Node tempNode = node.NextNode;
+                node.NextNode = newNode;
+                tempNode.PrevNode = newNode;
+                newNode.PrevNode = node;
+                newNode.NextNode = tempNode;
+            }
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            Node currentNode = this.head;
+            do
+            {
+                if (currentNode.Value == searchValue)
+                {
+                    return currentNode;
+                }
+                currentNode = currentNode.NextNode;
+            } while (currentNode != null);
+            return null;
+        }
+
+        public Node GetNode(int index)
+        {
+            if (index >= this.GetCount())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Node currentNode = this.head;
+            int count = 0;
+            while (count != index)
+            {
+                currentNode = currentNode.NextNode;
+                count++;
+            }
+            return currentNode;
+        }
+
+        public int GetCount()
+        {
+            int count = 0;
+            if (this.head != null)
+            {
+                Node currentNode = head;
+                count = 1;
+                while (currentNode.NextNode != null)
+                {
+                    currentNode = currentNode.NextNode;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public void RemoveNode(int index)
+        {
+            Node nodeToDelete = this.GetNode(index);
+            this.RemoveNode(nodeToDelete);
+        }
+
+        public void RemoveNode(Node node)
+        {
+            if (node == this.head)
+            {
+                if (node.NextNode == null)
+                {
+                    this.head = null;
+                }
+                else
+                {
+                    Node nextNode = node.NextNode;
+                    node.NextNode = null;
+                    nextNode.PrevNode = null;
+                    this.head = nextNode;
+                }
+            }
+            else
+            {
+                Node prevNode = node.PrevNode;
+                node.PrevNode = null;
+                if (node.NextNode == null)
+                {
+                    prevNode.NextNode = null;
+                }
+                else
+                {
+                    Node nextNode = node.NextNode;
+                    node.NextNode = null;
+                    prevNode.NextNode = nextNode;
+                    nextNode.PrevNode = prevNode;
+                }
+            }
+        }
+    }
+}
---
+++ Lesson_2_1/Program.cs
@@ -0,0 +1,267 @@
+﻿using System;
+
+namespace Lesson_2_1
+{
+    class Program
+    {
+        static bool CompareList(LinkedList list, int[] array)
+        {
+            if (list.GetCount() != array.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] != list.GetNode(i).Value)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        static void TestAddNode(TestCase testCase, int value)
+        {
+            bool validTest = false;
+            try
+            {
+                testCase.InputList.AddNode(value);
+                if (CompareList(testCase.InputList, testCase.ExpectedResult))
+                {
+                    validTest = true;
+                }
+                else
+                {
+                    validTest = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException == null)
+                {
+                    validTest = false;
+                }
+                else
+                {
+                    if (ex.GetType() == testCase.ExpectedException.GetType())
+                    {
+                        validTest = true;
+                    }
+                    else
+                    {
+                        validTest = false;
+                    }
+                }
+            }
+            finally
+            {
+                if (validTest)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("VALID");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FAILED");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.Write("Ожидаемый список: ");
+                PrintArray(testCase.ExpectedResult);
+                Console.Write($"Полученный список: ");
+                PrintNodes(testCase.InputList);
+                Console.WriteLine();
+            }
+        }
+        static void TestAddNodeAfter(TestCase testCase, Node node, int value)
+        {
+            bool validTest = false;
+            try
+            {
+                testCase.InputList.AddNodeAfter(node, value);
+                if (CompareList(testCase.InputList, testCase.ExpectedResult))
+                {
+                    validTest = true;
+                }
+                else
+                {
+                    validTest = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException == null)
+                {
+                    validTest = false;
+                }
+                else
+                {
+                    if (ex.GetType() == testCase.ExpectedException.GetType())
+                    {
+                        validTest = true;
+                    }
+                    else
+                    {
+                        validTest = false;
+                    }
+                }
+            }
+            finally
+            {
+                if (validTest)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("VALID");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FAILED");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.Write("Ожидаемый список: ");
+                PrintArray(testCase.ExpectedResult);
+                Console.Write($"Полученный список: ");
+                PrintNodes(testCase.InputList);
+                Console.WriteLine();
+            }
+        }
+        static void TestRemoveNode(TestCase testCase, int index)
+        {
+            bool validTest = false;
+            try
+            {
+                testCase.InputList.RemoveNode(index);
+                if (CompareList(testCase.InputList, testCase.ExpectedResult))
+                {
+                    validTest = true;
+                }
+                else
+                {
+                    validTest = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException == null)
+                {
+                    validTest = false;
+                }
+                else
+                {
+                    if (ex.GetType() == testCase.ExpectedException.GetType())
+                    {
+                        validTest = true;
+                    }
+                    else
+                    {
+                        validTest = false;
+                    }
+                }
+            }
+            finally
+            {
+                if (validTest)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("VALID");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("FAILED");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.Write("Ожидаемый список: ");
+                PrintArray(testCase.ExpectedResult);
+                Console.Write($"Полученный список: ");
+                PrintNodes(testCase.InputList);
+                Console.WriteLine();
+            }
+        }
+        static void PrintNodes(LinkedList list)
+        {
+            try
+            {
+                Node currentNode = list.GetNode(0);
+                do
+                {
+                    Console.Write($"{currentNode.Value} ");
+                    currentNode = currentNode.NextNode;
+                } while (currentNode != null);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Write("Cписок пуст");
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+        static void PrintArray(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                Console.Write("Cписок пуст");
+            }
+            else
+            {
+                foreach (int item in array)
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            LinkedList list = new LinkedList();
+            TestCase testCase; 
+            
+            //AddNode
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10 }, ExpectedException = null };
+            TestAddNode(testCase, 10);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20 }, ExpectedException = null };
+            TestAddNode(testCase, 20);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20 }, ExpectedException = null }; // Заведомо проваленный тест
+            TestAddNode(testCase, 30);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 40 }, ExpectedException = null };
+            TestAddNode(testCase, 40);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 40, 55 }, ExpectedException = null }; // Заведомо проваленный тест
+            TestAddNode(testCase, 50);
+
+            //AddNodeAfter
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 66, 40, 50 }, ExpectedException = null };
+            TestAddNodeAfter(testCase, testCase.InputList.GetNode(2), 66);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 66, 78, 40, 50 }, ExpectedException = null }; // Заведомо проваленный тест
+            TestAddNodeAfter(testCase, testCase.InputList.GetNode(3), 77);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 66, 77, 40, 50, 88 }, ExpectedException = null };
+            TestAddNodeAfter(testCase, testCase.InputList.GetNode(6), 88);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { -7, 10, 20, 30, 66, 77, 40, 50, 88 }, ExpectedException = null }; // Заведомо проваленный тест
+            TestAddNodeAfter(testCase, testCase.InputList.GetNode(0), -7);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, -8, -7, 20, 30, 66, 77, 40, 50, 88 }, ExpectedException = null };
+            TestAddNodeAfter(testCase, testCase.InputList.GetNode(0), -8);
+
+            //RemoveNode
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { -8, -7, 20, 30, 66, 77, 40, 50, 88 }, ExpectedException = null }; // Заведомо проваленный тест
+            TestRemoveNode(testCase, 1);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 66, 77, 40, 50, 88 }, ExpectedException = null };
+            TestRemoveNode(testCase, 1);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 77, 40, 50, 88 }, ExpectedException = null };
+            TestRemoveNode(testCase, 3);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 40, 50, 88 }, ExpectedException = null };
+            TestRemoveNode(testCase, 3);
+            testCase = new TestCase { InputList = list, ExpectedResult = new int[] { 10, 20, 30, 40, 50, 88 }, ExpectedException = null }; // Заведомо проваленный тест
+            TestRemoveNode(testCase, 5);
+        }
+    }
+}
