74a04bd21e8eb178232d27884e54dbc0a16fe81c
NickyParsons <79032032193@ya.ru>
2021-04-22 16:33:03 +0300

    Lesson 3 commit 1

---
+++ Lesson_3_1/BechmarkClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Lesson_3_1
+{
+    public class BechmarkClass
+    {
+        public static PointClass PointClassOne {get; set; }
+        public static PointClass PointClassTwo { get; set; }
+        public static PointStruct PointStructOne { get; set; }
+        public static PointStruct PointStructTwo { get; set; }
+        public BechmarkClass()
+        {
+            PointClassOne = new PointClass { X = 2, Y = 4 };
+            PointClassTwo = new PointClass { X = 37, Y = 15 };
+            PointStructOne = new PointStruct { X = 2, Y = 4 };
+            PointStructTwo = new PointStruct { X = 37, Y = 15 };
+        }
+
+        // PointClass — координаты типа float
+        public static float PointDistance(PointClass pointOne, PointClass pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        // PointStruct — координаты типа float
+        public static float PointDistance(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return MathF.Sqrt((x * x) + (y * y));
+        }
+
+        // PointStruct — координаты типа double
+        public static double PointDistanceDouble(PointStruct pointOne, PointStruct pointTwo)
+        {
+            double x = pointOne.X - pointTwo.X;
+            double y = pointOne.Y - pointTwo.Y;
+            return Math.Sqrt((x * x) + (y * y));
+        }
+
+        // PointStruct — координаты типа float (сокращенный)
+        public static float PointDistanceShort(PointStruct pointOne, PointStruct pointTwo)
+        {
+            float x = pointOne.X - pointTwo.X;
+            float y = pointOne.Y - pointTwo.Y;
+            return (x * x) + (y * y);
+        }
+
+        //ТЕСТЫ
+        [Benchmark]
+        public void TestClassFloat()
+        {
+            PointDistance(PointClassOne, PointClassTwo);
+        }
+
+        [Benchmark]
+        public void TestStructFloat()
+        {
+            PointDistance(PointStructOne, PointStructTwo);
+        }
+
+        [Benchmark]
+        public void TestStructDouble()
+        {
+            PointDistanceDouble(PointStructOne, PointStructTwo);
+        }
+
+        [Benchmark]
+        public void TestStructFloatShort()
+        {
+            PointDistanceShort(PointStructOne, PointStructTwo);
+        }
+
+    }
+}
---
+++ Lesson_3_1/PointClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_3_1
+{
+	public class PointClass
+	{
+		public int X;
+		public int Y;
+	}
+}
