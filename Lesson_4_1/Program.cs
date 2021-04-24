988caba0e95d5950841f5ee637ddd122ccad56e9
NickyParsons <79032032193@ya.ru>
2021-04-24 19:08:03 +0300

    Lesson 4 commit 1

---
+++ Lesson_4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[10000];
+            HashSet<string> hashSet = new HashSet<string>();
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                //string str = new string();
+            }
+        }
+    }
+}
