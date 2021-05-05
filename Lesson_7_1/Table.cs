c2b16efa6b9b2ceaa681efa417e7e2fa58e0bc54
NickyParsons <79032032193@ya.ru>
2021-05-05 15:50:22 +0300

    Lesson 7 commit 1

---
+++ Lesson_7_1/Table.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_7_1
+{
+    public static class Table
+    {
+        static string FormatCell(int number, int maxLenght)
+        {
+            string cell = Convert.ToString(number);
+            if (cell.Length < maxLenght)
+            {
+                return new string(' ', maxLenght - cell.Length) + cell;
+            }
+            else
+            {
+                return cell;
+            }
+        }
+        static void PrintHeader(int sizeX, int maxCellSize)
+        {
+            Console.Write('┌');
+            for (int i = 0; i < sizeX; i++)
+            {
+                Console.Write(new string('─', maxCellSize));
+                if (i != sizeX - 1)
+                {
+                    Console.Write('┬');
+                }
+                else
+                {
+                    Console.Write('┐');
+                }
+            }
+            Console.Write("\r\n");
+        }
+        static void PrintFooter(int sizeX, int maxCellSize)
+        {
+            Console.Write('└');
+            for (int i = 0; i < sizeX; i++)
+            {
+                Console.Write(new string('─', maxCellSize));
+                if (i != sizeX - 1)
+                {
+                    Console.Write('┴');
+                }
+                else
+                {
+                    Console.Write('┘');
+                }
+            }
+            Console.Write("\r\n");
+        }
+        static void PrintSeparator(int sizeX, int maxCellSize)
+        {
+            Console.Write('├');
+            for (int i = 0; i < sizeX; i++)
+            {
+                Console.Write(new string('─', maxCellSize));
+                if (i != sizeX - 1)
+                {
+                    Console.Write('┼');
+                }
+                else
+                {
+                    Console.Write('┤');
+                }
+            }
+            Console.Write("\r\n");
+        }
+        public static void PrintTable(int[,] a)
+        {
+            int lenghtX = a.GetLength(1);
+            int lenghtY = a.GetLength(0);
+            int maxCellSize = Convert.ToString(a[lenghtY - 1, lenghtX - 1]).Length;
+            PrintHeader(lenghtX, maxCellSize);
+            for (int i = 0; i < lenghtY; i++)
+            {
+                Console.Write('│');
+                for (int j = 0; j < lenghtX; j++)
+                {
+                    Console.Write(FormatCell(a[i, j], maxCellSize));
+                    Console.Write('│');
+                }
+                Console.Write("\r\n");
+                if (i != lenghtY - 1)
+                {
+                    PrintSeparator(lenghtX, maxCellSize);
+                }
+            }
+            PrintFooter(lenghtX, maxCellSize);
+        }
+    }
+}
---
+++ Lesson_7_1/TestCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_7_1
+{
+    public class TestCase
+    {
+        public int[ , ] InputArray { get; set; }
+        public int[ , ] ExpectedArray { get; set; }
+    }
+}
